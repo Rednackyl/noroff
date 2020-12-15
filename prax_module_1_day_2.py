6fe07468751840179bf2d689f0b7075a271e6e3e
Rednackyl <58472710+Rednackyl@users.noreply.github.com>
2020-12-15 12:18:59 +0100

    Add files via upload

---
+++ prax_module_1_day_2.py
@@ -0,0 +1,38 @@
+#### MODULE 1, DAY 2 ####
+
+### ACTIVITY 1 ###
+# To be PEP8 compliant, variables should be named with a single letter, word or words in a explicit and informative manner, lowercase characters and seperate words with underscores.
+
+# Candidates
+# a) WeeklySummary   ## Captial letters are unpractical in variable naming
+# b) the variable    ## Words should be separated with underscores.
+# c) myvalue         ## Words should be separated with underscores.
+# d) 1stvalue        ## Words should be separated with underscores.
+# e) 12345           ## Variables should be named with a letter, word or words
+# f) use_this_value  ## PEP8 Compliant, but should be more informative.
+# g) number1         ## Words should be separated with underscores.
+
+## Valid Candidate = f) ##
+
+
+### ACTIVITY 2 ###
+# MyHouse.Py
+print("  X")
+print(" XXX")
+print("XXXXX")
+print("|   |")
+print("|   |")
+print("-----")
+
+
+### ACTIVITY 3 ###
+user_name = input("Hello, please enter your name:")
+print ("Nice name!")
+user_birth_year = input("Please enter your year of birth:")
+print ("A legend was born ;)")
+user_birth_month = input("Please enter your month of birth:")
+print ("The month of coolness!")
+user_birth_day = input("Please enter your day of birth:")
+print ("I just love that date!")
+print ("So " + user_name + ", i have managed to find out that you were born " +  user_birth_day + "th in the month of " + user_birth_month + " in the year of " + user_birth_year + ", isnt that cool? :D")
+print ("Input was: " + user_name + ";" + user_birth_year + ";" + user_birth_month + ";" + user_birth_day + ". Output Complete!")
